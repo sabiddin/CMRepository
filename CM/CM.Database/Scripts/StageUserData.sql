e331e789aacc9418bdba4158dab5ce29ec4c6fe7
sabiddin <abiddinsyed@gmail.com>
2018-04-22 03:07:05 -0400

    Added the database project

---
+++ CM/CM.Database/Scripts/StageUserData.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO tbl_User(
+			[Username],
+			[Password],
+			[Locked],
+			[DateAdded])
+VALUES (	'sabiddin'
+		   ,'test'
+		   ,0
+		   ,GETDATE())
---
+++ CM/CM.Database/dbo/Tables/tbl_User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tbl_User] (
+    [UserID]    INT           IDENTITY (1, 1) NOT NULL,
+    [Username]  NVARCHAR (50) NULL,
+    [Password]  NVARCHAR (50) NULL,
+    [Locked]    BIT           NULL,
+    [DateAdded] DATE          NULL,
+    PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
+
